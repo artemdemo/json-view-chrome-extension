deaab0ee5718f836e47c7293a3e32cf35929da5d
Artem Demo <artemdemo@gmail.com>
2026-06-04 21:42:42 +0300

    css

--- packages/extension/src/render.tsx
+++ packages/extension/src/render.tsx
@@ -2,15 +2,11 @@ import { BrowserStorage } from "@jview/storage";
 import { JsonViewer } from "@jview/view";
 import { createRoot } from "react-dom/client";
 
-export function renderJsonViewer(
-  jsonStr: string,
-  source: string,
-) {
-  document.documentElement.classList.add("json-view-extension-document");
-
+export function renderJsonViewer(jsonStr: string, source: string) {
   const root = document.createElement("div");
 
   document.body.replaceChildren(root);
+  
   createRoot(root).render(
     <BrowserStorage>
       <JsonViewer jsonStr={jsonStr} source={source} />
