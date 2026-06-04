652a390150a5b62881ebb6f56258b0ddebcef66e
Artem Demo <artemdemo@gmail.com>
2026-06-04 20:18:56 +0300

    upd

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,9 +1,5 @@
-export type {
-  StorageContextValue,
-  UserSettingsChangeListener,
-} from './StorageProvider';
+export type { UserSettingsChangeListener } from './StorageProvider';
 export { StorageProvider } from './StorageProvider';
-export * from './storage';
 export { BrowserStorageStrategy } from './strategies/BrowserStorageStrategy';
 export type { IStorageStrategy } from './strategies/IStorageStrategy';
 export { useStorage } from './useStorage';
