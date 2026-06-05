bb10eb7bbe4ffefb117c67609416d65717ea5c10
Artem Demo <artemdemo@gmail.com>
2026-06-05 21:24:30 +0300

    memory storage

---
+++ packages/storage/src/MemoryStorage.tsx
@@ -0,0 +1,46 @@
+import type { UserSettings } from '@jview/definitions';
+import type { ComponentChildren } from 'preact';
+import { useMemo } from 'preact/hooks';
+import { StorageProvider } from '../../../storage/src/StorageProvider';
+import type { IStorageStrategy } from '../../../storage/src/strategies/IStorageStrategy';
+
+type MemoryStorageProps = {
+  children: ComponentChildren;
+  initialSettings?: UserSettings;
+};
+
+const defaultTestUserSettings: UserSettings = {
+  wordWrap: false,
+};
+
+// ToDo: Should be in `@jview/storage`
+class MemoryStorageStrategy implements IStorageStrategy {
+  readonly initialSettings: UserSettings;
+
+  #settings: UserSettings;
+
+  constructor(initialSettings: UserSettings) {
+    this.initialSettings = initialSettings;
+    this.#settings = initialSettings;
+  }
+
+  async loadUserSettings(): Promise<UserSettings> {
+    return this.#settings;
+  }
+
+  async saveUserSettings(settings: UserSettings): Promise<void> {
+    this.#settings = settings;
+  }
+}
+
+export const MemoryStorage = ({
+  children,
+  initialSettings = defaultTestUserSettings,
+}: MemoryStorageProps) => {
+  const strategy = useMemo(
+    () => new MemoryStorageStrategy(initialSettings),
+    [initialSettings],
+  );
+
+  return <StorageProvider strategy={strategy}>{children}</StorageProvider>;
+};
