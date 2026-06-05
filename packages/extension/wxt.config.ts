601268db12f9497d354703cca31059935e935a49
Artem Demo <artemdemo@gmail.com>
2026-06-05 15:05:58 +0300

    icon

--- packages/extension/wxt.config.ts
+++ packages/extension/wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  modules: ['@wxt-dev/auto-icons'],
   outDir: 'output',
   vite: () => ({
     esbuild: {
